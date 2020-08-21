be829bea147c9d4805142cee5ea058dce3619b0e
LovelyAnNaNa <2806121640@qq.com>
2020-08-21 17:16:53 +0800

    第一次提交

---
+++ src/main/java/com/xihe/appoint_inspect/AppointInspectApplication.java
@@ -0,0 +1,13 @@
+package com.xihe.appoint_inspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointInspectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointInspectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xihe/appoint_inspect/AppointInspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xihe.appoint_inspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointInspectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
